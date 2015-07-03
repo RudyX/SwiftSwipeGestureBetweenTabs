15cd716b82b7726bf0a974f319b028ecd7da9ae4
Özgür Celebi <oezguercelebi@gmail.com>
2015-07-03 14:53:43 +0200

    Initial Commit

---
+++ SwipeBetweenTabs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwipeBetweenTabs
+//
+//  Created by Özgür Celebi on 03.07.15.
+//  Copyright (c) 2015 Özgür Celebi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
