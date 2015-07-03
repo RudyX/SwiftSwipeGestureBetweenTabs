58d1152fafbf9d295c6d3952528a537fcec46f8b
Özgür Celebi <oezguercelebi@gmail.com>
2015-07-03 15:32:36 +0200

    Good to go

---
+++ SwipeBetweenTabs/ThirdTabVC.swift
@@ -0,0 +1,38 @@
+//
+//  ThirdTabVC.swift
+//  SwipeBetweenTabs
+//
+//  Created by Özgür Celebi on 03.07.15.
+//  Copyright (c) 2015 Özgür Celebi. All rights reserved.
+//
+
+import UIKit
+
+class ThirdTabVC: UIViewController {
+
+    @IBAction func SwipeGestureRight(sender: AnyObject) {
+        tabBarController?.selectedIndex = 1
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
